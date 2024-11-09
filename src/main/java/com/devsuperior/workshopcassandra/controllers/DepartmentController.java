495a57e260bda78694aad21867dded770de9b08a
icaro-freitas <icaro_freitas2014@outlook.com>
2024-11-09 16:50:37 -0300

    UPDATE department

--- src/main/java/com/devsuperior/workshopcassandra/controllers/DepartmentController.java
+++ src/main/java/com/devsuperior/workshopcassandra/controllers/DepartmentController.java
@@ -8,15 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.workshopcassandra.model.dto.DepartmentDTO;
 import com.devsuperior.workshopcassandra.services.DepartmentService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 
 @RestController
 @RequestMapping("/departments")
@@ -36,14 +36,19 @@ public class DepartmentController {
 		DepartmentDTO result = service.findById(id);
 		return ResponseEntity.ok(result);
 	}
-	
+
 	@PostMapping
 	public ResponseEntity<DepartmentDTO> insert(@RequestBody DepartmentDTO dto) {
 		dto = service.insert(dto);
-		
+
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
 		return ResponseEntity.created(uri).body(dto);
 	}
-	
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<DepartmentDTO> update(@PathVariable UUID id, @RequestBody DepartmentDTO dto) {
+		dto = service.update(id, dto);
+		return ResponseEntity.ok(dto);
+	}
 
 }
