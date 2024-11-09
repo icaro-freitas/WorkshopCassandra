7815ee7e2888a8df034f40f8aa6d9564f37c8efa
icaro-freitas <icaro_freitas2014@outlook.com>
2024-11-08 21:46:41 -0300

    GET Department by id

--- src/main/java/com/devsuperior/workshopcassandra/services/DepartmentService.java
+++ src/main/java/com/devsuperior/workshopcassandra/services/DepartmentService.java
@@ -1,6 +1,8 @@
 package com.devsuperior.workshopcassandra.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,16 +11,23 @@ import org.springframework.stereotype.Service;
 import com.devsuperior.workshopcassandra.model.dto.DepartmentDTO;
 import com.devsuperior.workshopcassandra.model.entities.Department;
 import com.devsuperior.workshopcassandra.repositories.DepartmentRepository;
+import com.devsuperior.workshopcassandra.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class DepartmentService {
-	
+
 	@Autowired
 	private DepartmentRepository repository;
-	
-	public List<DepartmentDTO> findAll(){
-		List<Department> list =  repository.findAll();
-		return list.stream().map(x->new DepartmentDTO(x)).collect(Collectors.toList());
-	} 
+
+	public List<DepartmentDTO> findAll() {
+		List<Department> list = repository.findAll();
+		return list.stream().map(x -> new DepartmentDTO(x)).collect(Collectors.toList());
+	}
+
+	public DepartmentDTO findById(UUID id) {
+		Optional<Department> result = repository.findById(id);
+		Department entity = result.orElseThrow(() -> new ResourceNotFoundException("Id não encontrado"));
+		return new DepartmentDTO(entity);
+	}
 
 }
